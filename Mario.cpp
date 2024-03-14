49b42e7e829b015a053bf07f042d9a56d8391391
Alex-Lark <143771535+Alex-Lark@users.noreply.github.com>
2024-03-13 21:57:03 -0700

    Added a lot of functions to Mario

--- Mario.cpp
+++ Mario.cpp
@@ -1,6 +1,108 @@
+#include <cstdlib> 
+#include <ctime>
+
 #include "Mario.h"
 
-
-Mario::Mario(int level, int locationX, int locationY) : GameObject(level, locationX, locationY){
+Mario::Mario(int level, int locationX, int locationY, int V) : GameObject(level, locationX, locationY){
     displayCharacter = 'H';
+    V = V;
+    coins = 0;
+    powerLevel = 0;
+    enemiesDefeated = 0;
 }
+
+bool Mario::interactWithObject(GameObject gameObject){
+    switch (gameObject.getDisplayCharacter()){ //tests type of object based on display character
+        case 'm':
+            return interactWithMushroom(gameObject);
+        case 'c':
+            return interactWithCoin(gameObject);
+        case 'w':
+            return interactWithWarpPipe(gameObject); //TODO: include interact with gameenemy here?
+        default:
+            return false;
+            //TODO: throw error
+    }
+}
+
+bool Mario::interactWithMushroom(GameObject mushroom){
+    if (powerLevel != 2){
+        powerLevel++;
+    }
+    return true;
+}
+
+bool Mario::interactWithCoin(GameObject coin){
+    if (++coins == 20){
+        coins = 0;
+        V++;
+    }
+    else{
+        coins++;
+    }
+    return true;
+}
+
+bool Mario::interactWithWarpPipe(GameObject warpPipe){
+    //TODO: IMPLEMENT
+    return true;
+}
+
+bool Mario::interactWithEnemy(GameEnemy enemy){
+    int successChance = rand() % 101; //TODO: improve random number?
+    if (successChance > enemy.getwinPercent()){ //mario Wins
+        marioDefeatsEnemy(enemy);
+        return true;
+    }
+    else{ //mario looses
+        if (enemy.getDisplayCharacter() == 'b'){
+            marioLosesToBoss();
+        }
+        else{
+            marioLosesToEnemy();
+        }
+        return false;
+    }
+}
+
+void Mario::marioDefeatsEnemy(GameEnemy enemy){
+    if (++enemiesDefeated == 7){ //adds another life if mario has defeated 7 enemies
+        V++;
+        enemiesDefeated = 0;
+    }
+    if (enemy.getDisplayCharacter() == 'b'){ //mario has defeated a boss
+        marioDefeatsBoss(enemy);
+    }
+}
+
+void Mario::marioDefeatsBoss(GameEnemy boss){
+    //TODO
+}
+
+void Mario::marioLosesToBoss(){
+    if (powerLevel == 2){ //reduces powerLevel
+        powerLevel = 0;
+    }
+    else{
+        marioLosesALife();
+    }
+}
+
+void Mario::marioLosesToEnemy(){
+    if (powerLevel > 0){ //reduces powerLevel
+        powerLevel--;
+    }
+    else{
+        marioLosesALife();
+    }
+}
+
+void Mario::marioLosesALife(){
+    V--;
+    powerLevel = 0;
+    enemiesDefeated = 0;
+}
+
+
+
+
--- Mario.h
+++ Mario.h
@@ -2,11 +2,35 @@
 #define MARIO_H
 
 #include "GameObject.h"
+#include "GameEnemy.h"
 
 class Mario: public GameObject{
 
 public: 
-    Mario(int level, int locationX, int locationY); //overloaded constructor
+    Mario(int level, int locationX, int locationY, int V); //overloaded constructor
+
+    //primary methods
+    bool interactWithObject(GameObject gameObject); //mario interacting with other objects, returns if that object should be deleted
+    bool interactWithEnemy(GameEnemy enemy); //TODO: integrate into interactWithObject?
+    //void Move(); TODO
+
+private:
+    int V; //how many lives Mario has
+    int coins; //how many coins mario has
+    int powerLevel; //mario's power level: 0, 1, or 2
+    int enemiesDefeated; //how many enemies mario has defeated on the current life
+
+    //interact methods, returns if the object should be deleted
+    bool interactWithMushroom(GameObject mushroom); //TODO: deleteObject?
+    bool interactWithCoin(GameObject coin);
+    bool interactWithWarpPipe(GameObject warpPipe); //TODO: implement 
+
+    //aux methods
+    void marioDefeatsEnemy(GameEnemy gameEnemy);
+    void marioDefeatsBoss(GameEnemy boss); //object needed? //TO BE Implemented
+    void marioLosesToBoss(); //mario loses to a boss
+    void marioLosesToEnemy(); //mario loses to a normal enemy
+    void marioLosesALife(); //called when mario loses a life
 };
 
 #endif
