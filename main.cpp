351aca94450641e014a2f60a1daaf3336e7b27d4
Alex-Lark <143771535+Alex-Lark@users.noreply.github.com>
2024-03-13 21:57:38 -0700

    current Main used for testing

--- main.cpp
+++ main.cpp
@@ -4,12 +4,19 @@
 #include "mushroom.h"
 #include "coin.h"
 #include "WarpPipe.h"
+#include "Goomba.h"
+#include "Mario.h"
+#include "GameEnemy.h"
+#include "Koopa.h"
+#include "LevelBoss.h"
 
 int main(){
-    Game *testGame = new Game("input-spec.txt", "log.txt");
-    delete testGame; 
+    /*Game *testGame = new Game("input-spec.txt", "log.txt");
+    testGame->runGame();
+    delete testGame;*/
 
-    //WarpPipe *testMushroom = new WarpPipe(0, 0, 0);   
-    //std::cout << testMushroom->getDisplayCharacter() << std::endl;
-    //delete testMushroom;
+    LevelBoss *testGoomba = new LevelBoss(0,0,0);   
+    std::cout << testGoomba->getDisplayCharacter() << std::endl;
+    std::cout << testGoomba->getwinPercent() << std::endl;
+    delete testGoomba;
 }
